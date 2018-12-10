2e155a102f79531d8554b25df9b49b213ae6e7a3
Bryce CLERC <bclerc@e1r13p13.42.fr>
2018-12-10 13:12:15 +0100

    Update

--- get_next_line.c
+++ get_next_line.c
@@ -6,78 +6,74 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 16:54:17 by bclerc            #+#    #+#             */
-/*   Updated: 2018/12/10 10:40:51 by bclerc           ###   ########.fr       */
+/*   Updated: 2018/12/10 13:11:32 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include "libft/libft.h"
-#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 char   *set_line(char **line, char *save)
 {
     int i;
-    int b;
-    char *tmp;
+
     i = 0;
     while (save[i] != '\n' && save[i])
         i++;
     *line = ft_strndup(save, i);
+    if (save[i + 1])
         return  (ft_strdup(&save[i + 1]));
-    return (0);
+    else
+    {
+        save = NULL;
+        return (NULL);
+    }
 }
 
 int get_next_line(const int fd, char **line)
 {
     char buff[BUFF_SIZE + 1];
     char static *save;
-    char *tmp;
     int ret;
-    int last;
 
     ret = 0;
-    last = 0;
     while((ret = read(fd, buff, BUFF_SIZE)))
     {
          buff[ret] = '\0';
         if  (!save)
-            save = ft_strnew(ft_strlen(buff));
-        save = ft_strjoin(save, buff);
+        {
+            if (!(save = ft_strnew(ft_strlen(buff))))
+                return (-1);
+        }
+        if (!(save = ft_strjoin(save, buff)))
+            {
+                free(save);
+                return (-1);
+            }
         if (!ft_strchr(buff, '\n'))
             continue ;
-
-        save = set_line(line, save);
+       if (!(save = set_line(line, save)))
+            {
+                free(save);
+                return (0);
+            }
         break;
     }
+    if (ret && save)
+        return (1);
     if (ret == 0 && save)
-        save = set_line(line, save);
-    return (0);
-    
-
-
-}
-
-int main(int argc, char **argv)
-{
-    int fd;
-    char *tab;
-    int i;
-    (void)argc;
-    char **pt;
-    tab = "";
-    pt = &tab;
-    fd = open("test", O_RDONLY);
-    i = 0;
-    while (i < 20)
     {
-
-    get_next_line(fd, pt);
-    printf("Ligne : %s \n", *pt);
-    i++;
+        save = set_line(line, save);
+        return (1);
+    }
+    else if (ret == 0)
+    {
+        free(save);
+        return (0);
     }
     return (0);
 }
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,15 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 16:55:50 by bclerc            #+#    #+#             */
-/*   Updated: 2018/12/10 10:40:50 by bclerc           ###   ########.fr       */
+/*   Updated: 2018/12/10 12:24:28 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 98
+
+int     get_next_line(const int fd, char **line);
 
 #endif
