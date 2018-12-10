5e148a8d607dbb3ecd3eeb81856ce6f59436dae8
Bryce CLERC <bclerc@e1r13p13.42.fr>
2018-12-10 10:39:14 +0100

    update

--- get_next_line.c
+++ get_next_line.c
@@ -6,12 +6,13 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 16:54:17 by bclerc            #+#    #+#             */
-/*   Updated: 2018/12/07 13:39:57 by bclerc           ###   ########.fr       */
+/*   Updated: 2018/12/10 10:38:20 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/get_next_line.h"
 #include "../libft/libft.h"
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -26,7 +27,8 @@ char   *set_line(char **line, char *save)
     while (save[i] != '\n' && save[i])
         i++;
     *line = ft_strndup(save, i);
-    return(ft_strdup(&save[i + 1]));
+        return  (ft_strdup(&save[i + 1]));
+    return (0);
 }
 
 int get_next_line(const int fd, char **line)
@@ -41,32 +43,38 @@ int get_next_line(const int fd, char **line)
     last = 0;
     while((ret = read(fd, buff, BUFF_SIZE)))
     {
-        buff[ret] = 0;
-        if(!save)
-        save = ft_strnew(ft_strlen(buff));
+         buff[ret] = '\0';
+        if  (!save)
+            save = ft_strnew(ft_strlen(buff));
         save = ft_strjoin(save, buff);
         if (!ft_strchr(buff, '\n'))
-        {
             continue ;
-        }
+
         save = set_line(line, save);
         break;
     }
+    if (ret == 0 && save)
+        save = set_line(line, save);
     return (0);
+    
+
+
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
     int fd;
     char *tab;
     int i;
+    (void)argc;
     char **pt;
     tab = "";
     pt = &tab;
     fd = open("test", O_RDONLY);
     i = 0;
-    while (i < 10)
+    while (i < 20)
     {
+
     get_next_line(fd, pt);
     printf("Ligne : %s \n", *pt);
     i++;
