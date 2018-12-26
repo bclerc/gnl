4ea3ac06a44a4eff9794bfbb4ed9128ab4ae4332
Bryce CLERC <bclerc@e1r13p13.42.fr>
2018-12-26 10:43:36 +0100

    Correction des includes .h

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,15 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/14 16:55:50 by bclerc            #+#    #+#             */
-/*   Updated: 2018/12/12 16:24:33 by bclerc           ###   ########.fr       */
+/*   Updated: 2018/12/26 10:22:04 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 1000
+# define BUFF_SIZE 32
+#include "libft/libft.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
