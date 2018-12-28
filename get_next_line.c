68e5a8a2bf90232a84b66cc6f7786d09bc043ab7
Bryce CLERC <bclerc@e1r13p13.42.fr>
2018-12-28 12:31:22 +0100

    leak al a con

--- get_next_line.c
+++ get_next_line.c
@@ -6,78 +6,81 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 10:16:46 by bclerc            #+#    #+#             */
-/*   Updated: 2018/12/26 10:41:30 by bclerc           ###   ########.fr       */
+/*   Updated: 2018/12/28 12:31:08 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char   *set_line(char **line, char *save)
+char	*set_line(char **line, char *save)
 {
-    int i;
-    char *tmp;
+	int		i;
+	char	*tmp;
 
-    i = 0;
-    while (save[i] != '\n' && save[i])
-        i++;
-    *line = ft_strndup(save, i);
-    if (i < (int)ft_strlen(save))
-    {
-        if (!(tmp = ft_strdup(&save[i + 1])))
-        {
-            free(save);
-            return  (NULL);
-        }
-        free(save);
-        return (tmp);
-    }
-    else
-    {
-        save = NULL;
-        return (NULL);
-    }
+	i = 0;
+	while (save[i] != '\n' && save[i])
+		i++;
+	if (!(*line = ft_strndup(save, i)))
+		return (NULL);
+	if (i < (int)ft_strlen(save))
+	{
+		if (!(tmp = ft_strdup(&save[i + 1])))
+		{
+			free(save);
+			return (NULL);
+		}
+		free(save);
+		return (tmp);
+	}
+	else
+	{
+		save = NULL;
+		return (NULL);
+	}
 }
 
-int get_next_line(const int fd, char **line)
+
+
+int		get_next_line(const int fd, char **line)
 {
-    char buff[BUFF_SIZE + 1];
-    char static *save;
-    int ret;
+	char		buff[BUFF_SIZE + 1];
+	char static *save;
+	int			ret;
 
-    if(fd < 0)
-        return (-1);
-    while((ret = read(fd, buff, BUFF_SIZE)) > 0)
-    {
-         buff[ret] = '\0';
-        if  (!save)
-        {
-            if (!(save = ft_strnew(ft_strlen(buff))))
-                return (-1);
-        }
-        if (!(save = ft_strjoinfree(save, buff)))
-            {
-                free(save);
-                return (-1);
-            }
-        if (!ft_strchr(buff, '\n'))
-            continue ;
-       if (!(save = set_line(line, save)))
-                free(save);
-        break;
-    }
-    if (ret && save)
-        return (1);
-    if (ret == 0 && save)
-    {
-        save = set_line(line, save);
-        return (1);
-    }
-    else if (ret == 0)
-    {
-        free(save);
-        *line = NULL;
-        return (0);
-    }
-    return (1);
+	if (fd < 0 || BUFF_SIZE < 1 || !line)
+		return (-1);
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+
+	{
+		buff[ret] = '\0';
+		if (!save)
+		{
+			if (!(save = ft_strnew(0)))
+				return (-1);
+		}
+		if (!(save = ft_strjoinfree(save, buff)))
+		{
+			free(save);
+			return (-1);
+		}
+		if (!ft_strchr(buff, '\n'))
+			continue ;
+		if (!(save = set_line(line, save)))
+			free(save);
+		break ;
+	}
+	if (ret && save)
+		return (1);
+	if (ret == 0 && save)
+	{
+		save = set_line(line, save);
+		return (1);
+	}
+	else if (ret == 0)
+	{
+		free(save);
+		*line = NULL;
+		return (0);
+	}
+	return (1);
 }
-
