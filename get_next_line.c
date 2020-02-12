24ea95df06e20b7fb027f23f1596bd61930a3d9e
BuildTools <unconfigured@null.spigotmc.org>
2020-02-12 17:18:21 +0100

    La, ca marche. Normalement.

--- get_next_line.c
+++ get_next_line.c
@@ -6,25 +6,11 @@
 /*   By: bclerc <bclerc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/26 10:16:46 by bclerc            #+#    #+#             */
-/*   Updated: 2020/02/12 16:31:49 by bclerc           ###   ########.fr       */
+/*   Updated: 2020/02/12 17:15:37 by bclerc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-void	ft_bzero(void *s, size_t n)
-{
-	int				i;
-	unsigned char	*tmp;
-
-	i = 0;
-	tmp = s;
-	while (i < (int)n)
-	{
-		tmp[i] = 0;
-		i++;
-	}
-}
 
 int		ft_strlen(char *str)
 {
@@ -55,51 +41,53 @@ void	ft_strdel(char **as)
 	*as = NULL;
 }
 
-static int	readline(const int fd, char **buffer)
+int		readline(char **save, const int fd)
 {
-	char	tmp[BUFFER_SIZE + 1];
-	char	*old_tmp;
+	char	buff[BUFFER_SIZE + 1];
+	char	*pt;
 	int		ret;
 
-	if ((ret = read(fd, tmp, BUFFER_SIZE)) == -1)
-		return (-1);
-	tmp[ret] = '\0';
-	old_tmp = *buffer;
-	*buffer = ft_strjoin(*buffer, tmp);
-	ft_strdel(&old_tmp);
+	ret = read(fd, buff, BUFFER_SIZE);
+	buff[ret] = 0;
+	pt = *save;
+	*save = ft_strjoin(*save, buff);
+	ft_strdel(&pt);
 	return (ret);
 }
 
-int			clean_buffer(char **buffer,char *line)
+int		setline(char *pos, char **line, char **save)
 {
-	line = ft_strdup(*buffer);
-	ft_strdel(buffer);
+	*pos = '\0';
+	*line = ft_strdup(*save);
+	ft_memmove(*save, pos + 1, ft_strlen(pos + 1) + 1);
 	return (1);
 }
 
-int			get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
-	static char		*buffer = 0;
-	char			*end;
-	int				ret;
+	char static	*save;
+	char		*pos;
+	int			ret;
 
-	if (!line || fd < 0 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1 || !line || (read(fd, NULL, 0) < 0))
 		return (-1);
-	buffer = buffer == 0 ? ft_strdup("") : buffer;
-	ret = 1;
+	save = save ? save : ft_strdup("");
+	ret = 42;
 	while (ret > 0)
 	{
-		if ((end = ft_strchr(buffer, '\n')))
+		if ((pos = ft_strchr(save, '\n')))
 		{
-			*end = '\0';
-			*line = ft_strdup(buffer);
-			ft_memmove(buffer, end + 1, ft_strlen(end + 1) + 1);
-			return (0);
+			setline(pos, line, &save);
+			return (1);
 		}
-		ret = readline(fd, &buffer);
+		ret = readline(&save, fd);
 	}
-	if (ret == 0 && ft_strlen(buffer))
-		return (clean_buffer(&buffer, *line));
-	(*line) = ft_strdup("");
+	if (!ret && ft_strlen(save))
+	{
+		*line = ft_strdup(save);
+		ft_strdel(&save);
+		return (0);
+	}
+	*line = ft_strdup("");
 	return (ret);
 }
